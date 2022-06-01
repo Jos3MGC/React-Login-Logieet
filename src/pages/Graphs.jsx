5a3de49978c9bc3a85bb075d87544ea854a1d06b
JOSÉ MARÍA GONZÁLEZ CRUZ <98995632+Jos3MGC@users.noreply.github.com>
2022-06-01 17:26:20 -0500

    .

--- src/pages/Graphs.jsx
+++ src/pages/Graphs.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { useUser } from "../UserProvider";
+import Box from "../components/box/Box";
+import { useTheme, styled } from "@mui/system";
+import DoughnutChart from "../components/CircularChart/doughnutChart";
+
+const Container = styled('div')(({ theme }) => ({
+    margin: '30px',
+    [theme.breakpoints.down('sm')]: {
+        margin: '16px',
+    },
+}))
 
 export default function Graph() {
 
-    const userId = useUser();
+    const theme = useTheme()
 
     return(
-        <h1>
-            {userId.id}
-        </h1>
+        <Container>
+            <Box>
+                <DoughnutChart 
+                  height="350px"
+                  
+                />
+            </Box>
+        </Container>
     )
 }
